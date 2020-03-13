b702f9d8c3794ef3b093c184f292f0191827f2bc
Vuyisile Ndlovu <terrameijar@gmail.com>
2020-03-13 15:18:38 +0200

    Run Chrome in headless mode

--- invoices/tests/test_UI.py
+++ invoices/tests/test_UI.py
@@ -3,15 +3,19 @@ import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 class TestInvoicingApp(StaticLiveServerTestCase):
 
     def setUp(self):
         self.base_url = self.live_server_url
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         # Add chrome driver to sys PATH first
-        self.driver = webdriver.Chrome(
-            executable_path='/usr/local/bin/chromedriver')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
 
     def test_landing_page_load(self):
         self.driver.get(self.base_url)
